c8c6f10a6d24a821ce5746776184b0831f033389
Shivam Preet <shivam.preet@synapse.com>
2020-06-20 13:51:24 +0530

    Local branch commit .

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -16,6 +16,7 @@ namespace MyFirstProject
             Console.WriteLine("My Third Github Code");
             Console.WriteLine("My Third Github Code");
             Console.WriteLine("My Third Github Code");
+            Console.WriteLine("My Local branch Code Code Change");
 
         }
     }
